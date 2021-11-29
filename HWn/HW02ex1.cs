3d4be2e5fdd731c93dec05fe080da4526b013d41
Jaime Colman <mjackerjaime@gmail.com>
2021-11-29 01:27:26 -0300

    Added sub-program: Perimeter of a regular polygon.

--- HWn/HW02ex1.cs
+++ HWn/HW02ex1.cs
@@ -10,7 +10,33 @@ namespace HWn
     {
         public static void Main()
         {
+            // Exercice:
+            Console.WriteLine("Hacer un programa que calcule el perímetro de cualquier poligono regular");
+            Console.WriteLine("------------------------------------------------------------------------");
 
+            // Program:
+            Double s;
+            int n;
+                    Console.WriteLine("This Program calculate the perimeter of a regular polygon. \nA regular polygon consist in n-sides in witch the sides are all the same.");
+            while (true){
+                try
+                {
+                    Console.Write("Enter side dimension: ");
+                    s = Convert.ToDouble(Console.ReadLine());
+                    break;
+                }
+                catch (FormatException) {Console.WriteLine("\nInvalid format, please enter try again.");}
+            }
+            while (true){ 
+                    try
+                    {
+                        Console.Write("Enter n-sides: ");
+                        n = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    }
+                    catch (FormatException) {Console.WriteLine("\nInvalid format, please enter an integer number.");}
+            }
+            Console.WriteLine("The total perimeter is: {0}", s * (Convert.ToDouble(n)));
         }
     }
 }
