908779140b9fc94b4d35c3cf852b8fb35f5be853
Jaime Colman <mjackerjaime@gmail.com>
2021-12-03 01:02:55 -0300

    Added HW 5 - Methods:: 1) Celsius to radians, 2) Figures areas (circle, triangle, square).

--- HWn/HW02ex1.cs
+++ HWn/HW02ex1.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Globalization;
+using System.Threading;
 
 namespace HWn
 {
@@ -15,6 +17,9 @@ namespace HWn
             Console.WriteLine("------------------------------------------------------------------------");
 
             // Program:
+            // Aditional configuration for zone time.
+            System.Globalization.CultureInfo.CreateSpecificCulture("en-US");
+
             Double s;
             int n;
                     Console.WriteLine("This Program calculate the perimeter of a regular polygon. \nA regular polygon consist in n-sides in witch the sides are all the same.");
@@ -22,7 +27,7 @@ namespace HWn
                 try
                 {
                     Console.Write("Enter side dimension: ");
-                    s = Convert.ToDouble(Console.ReadLine());
+                    s = Convert.ToDouble(Console.ReadLine()); //2.2  //2,2
                     break;
                 }
                 catch (FormatException) {Console.WriteLine("\nInvalid format, please enter try again.");}
@@ -36,7 +41,17 @@ namespace HWn
                     }
                     catch (FormatException) {Console.WriteLine("\nInvalid format, please enter an integer number.");}
             }
-            Console.WriteLine("The total perimeter is: {0}", s * (Convert.ToDouble(n)));
+            Console.WriteLine("The total perimeter is: {0}", s * Convert.ToDouble(n));
+
+
+            // Aditional Info: Culture info
+            CultureInfo culture1 = CultureInfo.CurrentCulture;
+            CultureInfo culture2 = Thread.CurrentThread.CurrentCulture;
+            Console.WriteLine("\n\n-------------------------------------\n");
+            Console.WriteLine("The current culture is {0}", culture1.Name);
+            Console.WriteLine("The two CultureInfo objects are equal: {0}",
+                              culture1 == culture2);
+
         }
     }
 }
---
+++ HWn/HW05ex2.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* Temas que estoy abordando en este programa:
+ *  - Imprementacion de clases abstractas para calcular el area de figuras.
+ *  -
+ */
+
+namespace HWn
+{
+    class HW05ex2
+    {
+        public static void Main()
+        {
+            // Exercice:
+            Console.Write("Crear una aplicación que calcule el área de un círculo, cuadrado, o triangulo. " +
+                "Le preguntaremos al usuario a qué figura le quiere calcular el área y dependiendo el caso, ejecutará uno de los 3 métodos.\n--------------------------------------------------------------------");
+
+            // Program:
+            Console.Write("Choose a figure: \n---------------\n 1) Circle.\n 2) Square.\n 3) Triangle.\n>> ");
+            String op = Console.ReadLine();
+
+            try 
+            { 
+                switch (op)
+                {
+                    case "1":
+                        Circulo miCirculo = new Circulo();
+                        miCirculo.Area();
+                        break;
+                    case "2":
+                        Cuadrado miCuadrado = new Cuadrado();
+                        miCuadrado.Area();
+                        break;
+                    case "3":
+                        Triangulo miTriangulo = new Triangulo();
+                        miTriangulo.Area();
+                        break;
+                    default:
+                        Console.Write("Invalid option.");
+                        break;
+                }
+            }
+            catch (Exception a)
+            {
+                Console.Write("WARNING: {0}", a.Message);
+            }
+        }
+    }
+
+
+    // Abstract class
+    public abstract class Figures
+    { 
+        // Abstract method (does not have a body)
+        public abstract void Area();
+    };
+
+    // Derived class (inherit from Animal)
+    public class Triangulo : Figures
+    {
+        public override void Area()
+        {
+            // The body of Area() is provided here
+            Console.Write("Enter base: ");
+            Double b = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter hight: ");
+            Double h = Convert.ToDouble(Console.ReadLine());
+            Console.Write("\nThe area is: {0}", (b * h) / 2);
+        }
+    }
+
+    public class Cuadrado : Figures
+    {
+        public override void Area()
+        {
+            // The body of Area() is provided here
+            Console.Write("Enter side: ");
+            Double s = Convert.ToDouble(Console.ReadLine());
+            Console.Write("\nThe area is: {0}", s * s);
+        }
+    }
+
+    public class Circulo : Figures
+    {
+        public override void Area()
+        {
+            // The body of Area() is provided here
+            Console.Write("Enter radio: ");
+            Double r = Convert.ToDouble(Console.ReadLine());
+            Console.Write("\nThe area is: {0}", Math.PI * r * r);
+        }
+    }
+}
